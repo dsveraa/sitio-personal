355a5870cfa55901786ca4fde9feef9776903d47
David Vera <dsvera.a@gmail.com>
2024-04-28 23:53:03 -0400

    quité la lógica de scroll y touch

--- src/app/components/web/web.component.ts
+++ src/app/components/web/web.component.ts
@@ -8,93 +8,93 @@ import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
   styleUrl: './web.component.css'
 })
 export class WebComponent {
-  @ViewChild('contenedorMobile') contenedor!: ElementRef;
+//   @ViewChild('contenedorMobile') contenedor!: ElementRef;
 
-  isDragging = false;
-  startY!: number;
-  startScroll!: number;
-  momentumInterval: any;
-  velocity!: number;
+//   isDragging = false;
+//   startY!: number;
+//   startScroll!: number;
+//   momentumInterval: any;
+//   velocity!: number;
 
-  @HostListener('touchstart', ['$event'])
-  onTouchStart(event: TouchEvent) {
-    event.preventDefault();
-    this.isDragging = true;
-    this.startY = event.touches[0].clientY;
-    this.startScroll = this.contenedor.nativeElement.scrollTop;
-    this.clearMomentum();
-  }
+//   @HostListener('touchstart', ['$event'])
+//   onTouchStart(event: TouchEvent) {
+//     event.preventDefault();
+//     this.isDragging = true;
+//     this.startY = event.touches[0].clientY;
+//     this.startScroll = this.contenedor.nativeElement.scrollTop;
+//     this.clearMomentum();
+//   }
 
-  @HostListener('touchmove', ['$event'])
-  onTouchMove(event: TouchEvent) {
-    event.preventDefault(); 
-    if (this.isDragging) {
-      const deltaY = event.touches[0].clientY - this.startY;
-      this.contenedor.nativeElement.scrollTop = this.startScroll - deltaY;
-      this.velocity = deltaY / 5;
-    }
-  }
+//   @HostListener('touchmove', ['$event'])
+//   onTouchMove(event: TouchEvent) {
+//     event.preventDefault(); 
+//     if (this.isDragging) {
+//       const deltaY = event.touches[0].clientY - this.startY;
+//       this.contenedor.nativeElement.scrollTop = this.startScroll - deltaY;
+//       this.velocity = deltaY / 5;
+//     }
+//   }
 
-  @HostListener('touchend')
-  onTouchEnd() {
-    this.isDragging = false;
-    this.applyMomentum();
-  }
+//   @HostListener('touchend')
+//   onTouchEnd() {
+//     this.isDragging = false;
+//     this.applyMomentum();
+//   }
 
-  @HostListener('touchcancel')
-  onTouchCancel() {
-    this.isDragging = false;
-    this.applyMomentum();
-  }
+//   @HostListener('touchcancel')
+//   onTouchCancel() {
+//     this.isDragging = false;
+//     this.applyMomentum();
+//   }
 
-  onMouseDown(event: MouseEvent) {
-    event.preventDefault(); 
-    this.isDragging = true;
-    this.startY = event.clientY;
-    this.startScroll = this.contenedor.nativeElement.scrollTop;
-    this.clearMomentum();
-  }
+//   onMouseDown(event: MouseEvent) {
+//     event.preventDefault(); 
+//     this.isDragging = true;
+//     this.startY = event.clientY;
+//     this.startScroll = this.contenedor.nativeElement.scrollTop;
+//     this.clearMomentum();
+//   }
 
-  onMouseMove(event: MouseEvent) {
-    event.preventDefault(); 
-    if (this.isDragging) {
-      const deltaY = event.clientY - this.startY;
-      this.contenedor.nativeElement.scrollTop = this.startScroll - deltaY;
-      this.velocity = deltaY / 5; 
-    }
-  }
+//   onMouseMove(event: MouseEvent) {
+//     event.preventDefault(); 
+//     if (this.isDragging) {
+//       const deltaY = event.clientY - this.startY;
+//       this.contenedor.nativeElement.scrollTop = this.startScroll - deltaY;
+//       this.velocity = deltaY / 5; 
+//     }
+//   }
 
-  onMouseUp() {
-    this.isDragging = false;
-    this.applyMomentum();
-  }
+//   onMouseUp() {
+//     this.isDragging = false;
+//     this.applyMomentum();
+//   }
 
-  onMouseLeave() {
-    this.isDragging = false;
-    this.applyMomentum();
-  }
+//   onMouseLeave() {
+//     this.isDragging = false;
+//     this.applyMomentum();
+//   }
 
-  applyMomentum() {
-    const momentum = 0.9;
+//   applyMomentum() {
+//     const momentum = 0.9;
 
-    this.momentumInterval = setInterval(() => {
-      if (Math.abs(this.velocity) < 0.5) {
-        clearInterval(this.momentumInterval);
-        return;
-      }
+//     this.momentumInterval = setInterval(() => {
+//       if (Math.abs(this.velocity) < 0.5) {
+//         clearInterval(this.momentumInterval);
+//         return;
+//       }
 
-      this.velocity *= momentum;
-      this.contenedor.nativeElement.scrollTop -= this.velocity;
+//       this.velocity *= momentum;
+//       this.contenedor.nativeElement.scrollTop -= this.velocity;
 
-      if (this.contenedor.nativeElement.scrollTop <= 0 || 
-          this.contenedor.nativeElement.scrollTop >= this.contenedor.nativeElement.scrollHeight - this.contenedor.nativeElement.clientHeight) {
-        clearInterval(this.momentumInterval);
-      }
-    }, 16);
-  }
+//       if (this.contenedor.nativeElement.scrollTop <= 0 || 
+//           this.contenedor.nativeElement.scrollTop >= this.contenedor.nativeElement.scrollHeight - this.contenedor.nativeElement.clientHeight) {
+//         clearInterval(this.momentumInterval);
+//       }
+//     }, 16);
+//   }
 
-  clearMomentum() {
-    clearInterval(this.momentumInterval);
-  }
+//   clearMomentum() {
+//     clearInterval(this.momentumInterval);
+//   }
 }
 
