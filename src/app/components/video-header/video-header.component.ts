89c4321a10e93af83a89cf2d1e013ef4464c13f1
David Vera <dsvera.a@gmail.com>
2024-05-04 13:29:20 -0400

    loader

--- src/app/components/video-header/video-header.component.ts
+++ src/app/components/video-header/video-header.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { VideoLoaderComponent } from '../video-loader/video-loader.component';
 
 @Component({
   selector: 'app-video-header',
   standalone: true,
-  imports: [],
+  imports: [VideoLoaderComponent],
   templateUrl: './video-header.component.html',
   styleUrl: './video-header.component.css'
 })
 export class VideoHeaderComponent {
+  showLoader = true;
 
+  videoLoaded() {
+    this.showLoader = false; 
+  }
 }
