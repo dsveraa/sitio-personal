c681572c42d196d10db08eb67dab78d9a8b1e54e
David Vera <dsvera.a@gmail.com>
2024-11-27 11:40:49 -0300

    modificación para sitio para modo looking-for-a-job

--- src/app/components/invitacion/invitacion.component.ts
+++ src/app/components/invitacion/invitacion.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Cinema4dComponent } from '../icons/cinema4d/cinema4d.component';
+import { IconsSizeService } from '../../services/icons-size.service';
+
 
 @Component({
   selector: 'app-invitacion',
   standalone: true,
-  imports: [],
+  imports: [
+    Cinema4dComponent
+  ],
   templateUrl: './invitacion.component.html',
   styleUrl: './invitacion.component.css'
 })
-export class InvitacionComponent {
+export class InvitacionComponent implements OnInit{
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private _iconSize: IconsSizeService,
+  ) { }
 
+  iconSize: any;
+
+  ngOnInit() {
+    this.iconSize = this._iconSize.iconsSize;
+  }
 }
